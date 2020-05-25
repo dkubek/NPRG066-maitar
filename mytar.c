3bbef770d7b8c982e7b8bf9c5ca81d67045f6a9a
David Kubek <david.kubek@gmail.com>
2020-05-25 16:56:45 +0200

    Add support for -f and -v options

--- mytar.c
+++ mytar.c
@@ -5,15 +5,22 @@
 
 #define	LIST_OPT 't'
 #define	ARCHIVE_OPT 'f'
+#define	VERBOSE_OPT 'v'
+#define	EXTRACT_OPT 'x'
 
 #define	LIST_FLAG 0x01
 #define	ARCHIVE_FLAG 0x02
+#define	EXTRACT_FLAG 0x04
+#define	VERBOSE_FLAG 0x08
 
 #define	BLOCK_SIZE 512
 #define	HEADER_SIZE 500
 
 #define	REGTYPE '0'	/* regular file */
 #define	AREGTYPE '\0'	/* regular file */
+
+#define	TMAGIC "ustar "	/* ustar space and a null */
+#define	TMAGLEN 6
 
 const char USAGE_STR[] = "usage: %s -t [ file1 file2 ... ] -f ARCHIVE ";
 
@@ -64,9 +71,16 @@ typedef struct
 
 Args * args_new();
 void args_free(Args *args);
+
+Archive * archive_open(char *archive_fname);
+int is_tar_archive(FILE *fp);
+void archive_close(Archive *arch);
+
 void process_args(int argc, char *argv[], Args *args);
 void process_archive_arg(char ***argv, Args* args);
 void process_list_arg(Args* args);
+void process_verbose_arg(Args* args);
+void process_extract_arg(Args* args);
 void validate_args(Args* args);
 
 int next_entry(Archive *arch, Entry *ent);
@@ -75,6 +89,7 @@ int is_empty_block(char *arr, size_t size);
 void list(Archive *arch, char *list_files[], int lf_count);
 int remove_str(char *arr[], int size, char *str);
 int report_missing(char *files[], int size);
+void extract(Archive *arch, char *files[], int count, int verbose);
 
 
 Args *
@@ -125,6 +140,12 @@ process_args(int argc, char *argv[], Args *args)
 			case ARCHIVE_OPT:
 				process_archive_arg(&p, args);
 				break;
+			case VERBOSE_OPT:
+				process_verbose_arg(args);
+				break;
+			case EXTRACT_OPT:
+				process_extract_arg(args);
+				break;
 			default:
 				errx(2, "invalid option -- '%c'", shortarg);
 		}
@@ -154,8 +175,7 @@ process_archive_arg(char **pargv[], Args* args)
 }
 
 /*
- * Process arguments associated with the -t file flag. Leave pointer on
- * the next unused argument.
+ * Process arguments associated with the -t file flag.
  */
 void
 process_list_arg(Args* args)
@@ -164,6 +184,27 @@ process_list_arg(Args* args)
 		errx(2, "Multiple uses of -t not supported");
 
 	args->flags = args->flags | LIST_FLAG;
+}
+
+/*
+ * Process arguments associated with the -v file flag.
+ */
+void
+process_verbose_arg(Args* args)
+{
+	if (args->flags & VERBOSE_FLAG)
+		errx(2, "Multiple uses of -v not supported");
+
+	args->flags = args->flags | VERBOSE_FLAG;
+}
+
+/*
+ * Process arguments associated with the -v file flag.
+ */
+void
+process_extract_arg(Args* args)
+{
+	args->flags = args->flags | EXTRACT_FLAG;
 }
 
 /*
@@ -176,10 +217,16 @@ validate_args(Args* args)
 		errx(2, "Refusing to read archive contents from terminal"
 			"(missing -f option?)");
 
-	if (!(args->flags & LIST_FLAG))
-		errx(2, "You must specify the -t options.");
+	if (!(args->flags & LIST_FLAG) && !(args->flags & EXTRACT_FLAG))
+		errx(2, "You must specify one of the -tx options.");
+
+	if ((args->flags & LIST_FLAG) && (args->flags & EXTRACT_FLAG))
+		errx(2, "You may not specify more than one -tx options.");
 }
 
+/*
+ * Crate a new archive object.
+ */
 Archive *
 archive_open(char *archive_fname)
 {
@@ -191,6 +238,9 @@ archive_open(char *archive_fname)
 	if ((arch = malloc(sizeof (Archive))) == NULL)
 		err(2, "malloc");
 
+	if (!is_tar_archive(fp))
+		return (NULL);
+
 	fseek(fp, 0L, SEEK_END);
 	arch->len = ftell(fp);
 	fseek(fp, 0L, SEEK_SET);
@@ -201,6 +251,33 @@ archive_open(char *archive_fname)
 	return (arch);
 }
 
+/*
+ * Check if given file is a tar archive.
+ */
+int
+is_tar_archive(FILE *fp)
+{
+	rewind(fp);
+
+	struct posix_header header;
+
+	int items_read = fread(&header, sizeof (struct posix_header), 1, fp);
+
+	if (items_read == 0)
+		return (0);
+
+	// Check magic field
+	if (strncmp(TMAGIC, header.magic, TMAGLEN)) {
+		warnx("This does not look like a tar archive");
+		return (0);
+	}
+
+	return (1);
+}
+
+/*
+ * Safely close archive.
+ */
 void
 archive_close(Archive *arch)
 {
@@ -326,14 +403,18 @@ list(Archive *arch, char *list_files[], int lf_count)
 		status = next_entry(arch, ent);
 	}
 
-	if (status == 2)
+	if (status == 2) {
+		free(ent);
 		errx(2, "Error is not recoverable: exiting now");
+	}
 
-	// List files not found in archive
+		// List files not found in archive
 	if (lf_count != 0)
-		if (report_missing(list_files, lf_count))
+		if (report_missing(list_files, lf_count)) {
+			free(ent);
 			errx(2, "Exiting with failure status due to previous "
 				"errors");
+		}
 
 	free(ent);
 }
@@ -370,14 +451,100 @@ report_missing(char *files[], int size)
 	return (found);
 }
 
+/*
+ * Extract file pointed to by ent from archive. Extract as much data as
+ * possible. File is created with the filename specified in the archive.
+ *
+ * Returns number of bytes written.
+ */
+int
+extract_file(Archive *arch, Entry *ent)
+{
+	char *filename = (ent->header).name;
+	char **endptr = NULL;
+	int data_size = strtol(ent->header.size, endptr, 8);
+
+	FILE *fout;
+	if ((fout = fopen(filename, "w")) == NULL)
+		err(2, "%s: Cannot open for writing", filename);
+
+	FILE *fin = arch->fp;
+	fseek(fin, ent->data_offset, SEEK_SET);
+
+	char *buffer;
+	if ((buffer = malloc(data_size)) == NULL)
+		err(2, "malloc");
+
+	int bytes_read = fread(buffer, sizeof (char), data_size, fin);
+
+	int bytes_written = fwrite(buffer, sizeof (char), bytes_read, fout);
+
+	free(buffer);
+	fclose(fout);
+	return (bytes_written);
+}
+
+/*
+ * Extract specified files from archive. If verbose is non-zero print
+ * currently extracted file to stdout.
+ */
+void
+extract(Archive *arch, char *files[], int count, int verbose)
+{
+	Entry *ent;
+	if ((ent = malloc(sizeof (Entry))) == NULL)
+		err(2, "malloc");
+
+	int status;
+	status = next_entry(arch, ent);
+	while (status == 1) {
+		if (ent->header.typeflag != REGTYPE &&
+		    ent->header.typeflag != AREGTYPE) {
+			errx(2, "Unsupported header type: %d",
+				ent->header.typeflag);
+		}
+
+		if (count == 0) {
+			if (verbose)
+				printf("%s\n", ent->header.name);
+
+			extract_file(arch, ent);
+		} else if (remove_str(files, count, ent->header.name)) {
+			if (verbose)
+				printf("%s\n", ent->header.name);
+
+			extract_file(arch, ent);
+		}
+
+		fflush(stdout);
+
+		status = next_entry(arch, ent);
+	}
+
+	if (status == 2)
+		errx(2, "Error is not recoverable: exiting now");
+
+	free(ent);
+}
+
 int
 main(int argc, char *argv[])
 {
 	Args *args = args_new();
 	process_args(argc, argv, args);
 
+	int verbose = 0;
+	if (args->flags & VERBOSE_FLAG)
+		verbose = 1;
+
 	Archive* arch = archive_open(args->archive);
-	list(arch, args->files, args->len);
+	if (arch == NULL)
+		errx(2, "Exiting with failure status due to previous errors");
+
+	if (args->flags & LIST_FLAG)
+		list(arch, args->files, args->len);
+	else if (args->flags & EXTRACT_FLAG)
+		extract(arch, args->files, args->len, verbose);
 
 	archive_close(arch);
 	args_free(args);
